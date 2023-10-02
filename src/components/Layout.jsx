9eadf85bf5819d31d26e730fead7e2bf1d38f621
Gabriel Godoy <gabyreload@gmail.com>
2023-10-02 16:44:22 -0300

    Change layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import SideBar from "./SideBar";
 
 export default function Layout() {
   return (
     <div className="site-wrapper">
-      <NavBar />
-      <div className="container ">
-        <Outlet />
+      <div className="container-fluid m-0 p-0">
+        <NavBar />
+        <div className="row m-0">
+          <div className="col-md-3 col-lg-2 bg-dark p-2">
+            <SideBar />
+          </div>
+          <div className="col-md-9 col-lg-10">
+            <Outlet />
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
