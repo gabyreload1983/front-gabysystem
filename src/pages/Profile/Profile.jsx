89179625122a277bb9b1a038404acd506b1d72f4
Gabriel Godoy <gabyreload@gmail.com>
2023-11-29 12:22:06 -0300

    update image user profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,17 +1,91 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/userContext";
+import { SwalSuccess, SwalWaiting } from "../../utils";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 export default function Profile() {
   const { user } = useContext(UserContext);
+
+  const handleUpdateImageUrl = async () => {
+    const { value: imageUrl } = await Swal.fire({
+      title: "Ingrese URL de imagen de perfil",
+      input: "text",
+      inputLabel: "URL",
+      showCancelButton: true,
+      inputValidator: (value) => {
+        if (!value) {
+          return "Ingrese URL de imagen de perfil";
+        }
+      },
+    });
+    if (imageUrl) {
+      SwalWaiting("Actualizando perfil...");
+
+      const res = await axios.post(
+        `http://${import.meta.env.VITE_URL_HOST}/api/users/image-url`,
+        { imageUrl },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          },
+        }
+      );
+
+      return SwalSuccess(res.data.message);
+    }
+  };
   return (
     <div className="container">
       {user && (
-        <div>
-          <p>Nombre: {user.first_name}</p>
-          <p>Apellido: {user.last_name}</p>
-          <p>Email: {user.email}</p>
-          <p>Codigo Urbano: {user.code_technical}</p>
-          <p>Rol: {user.role}</p>
+        <div
+          className="row border border-3 border-success p-4 rounded-5 mt-3"
+          style={{ maxWidth: 600 }}
+        >
+          <div className="col d-flex flex-column justify-content-center align-items-center">
+            {user?.imageUrl ? (
+              <img
+                className="img-profile"
+                src="https://avatars.githubusercontent.com/u/66527583?v=4"
+                alt=""
+              />
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="200"
+                height="200"
+                fill="currentColor"
+                className="bi bi-person-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                <path
+                  fillRule="evenodd"
+                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                />
+              </svg>
+            )}
+            <svg
+              onClick={handleUpdateImageUrl}
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-pencil mt-3 cursor-pointer"
+              viewBox="0 0 16 16"
+            >
+              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+            </svg>
+          </div>
+          <div className="col d-flex flex-column">
+            <p className="border-bottom border-2">NOMBRE: {user.first_name}</p>
+            <p className="border-bottom border-2">APELLIDO: {user.last_name}</p>
+            <p className="border-bottom border-2">EMAIL: {user.email}</p>
+            <p className="border-bottom border-2">
+              CODIGO URBANO: {user.code_technical}
+            </p>
+            <p className="border-bottom border-2">ROL: {user.role}</p>
+          </div>
         </div>
       )}
     </div>
