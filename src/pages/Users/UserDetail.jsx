8e7b4ae7e2f12d64a4e64bde10229448d51f9ac6
Gabriel Godoy <gabyreload@gmail.com>
2024-01-02 10:24:11 -0300

    Feature/issue 6.2.0/admin panel (#34)
    * update users admin panel
    
    * fix className
    
    * change user icon color
    
    * update package.json version

--- src/pages/Users/UserDetail.jsx
+++ src/pages/Users/UserDetail.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { SwalError, getFromApi, putToApi, validateStatus } from "../../utils";
 import Swal from "sweetalert2";
 import { UserContext } from "../../context/userContext";
+import axios from "axios";
 
 export default function UserDetail() {
   const { id } = useParams();
@@ -66,81 +67,165 @@ export default function UserDetail() {
     }
   };
 
+  const remove = async (user) => {
+    try {
+      const question = await Swal.fire({
+        text: `Borrar usuario ${user.first_name} ${user.last_name}???`,
+        showCancelButton: true,
+        confirmButtonText: "Aceptar",
+      });
+      if (!question.isConfirmed) return;
+
+      const { data } = await axios.delete(
+        `http://${import.meta.env.VITE_URL_HOST}/api/users/${user.email}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          },
+        }
+      );
+
+      if (validateStatus(data) === "jwt-expired") {
+        logoutUserContext();
+        return navigate("/login");
+      }
+
+      if (data.status === "success") {
+        await Swal.fire({
+          toast: true,
+          icon: "success",
+          text: data.message,
+          position: "top-end",
+          timer: 1000,
+          showConfirmButton: false,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        });
+        navigate("/users");
+      }
+    } catch (error) {
+      SwalError(error);
+    }
+  };
+
   return (
-    <div className="container">
-      <div className="row my-3 justify-content-center">
-        <div className="col-12 col-md-6 col-lg-4">
-          {userUpdate && (
-            <form id="formRegister">
-              <div className="form-floating mb-3">
-                <input
-                  className="form-control form-control-sm"
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Nombre"
-                  name="first_name"
-                  required
-                  value={userUpdate.first_name}
-                />
-                <label htmlFor="first_name">Nombre</label>
-              </div>
+    <div className="row my-3">
+      <div className="col-12 col-md-6 col-lg-4">
+        {userUpdate?.imageUrl ? (
+          <img className="img-fluid" src={userUpdate.imageUrl} alt="" />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            className="bi bi-person-circle"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+            <path
+              fillRule="evenodd"
+              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+            />
+          </svg>
+        )}
+      </div>
+      <div className="col-12 col-md-6 col-lg-4">
+        {userUpdate && (
+          <>
+            <div className="form-floating mb-3">
+              <input
+                className="form-control form-control-sm"
+                onChange={handleChange}
+                type="text"
+                placeholder="Nombre"
+                name="first_name"
+                required
+                value={userUpdate.first_name}
+              />
+              <label htmlFor="first_name">Nombre</label>
+            </div>
 
-              <div className="form-floating mb-3">
-                <input
-                  className="form-control form-control-sm"
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Apellido"
-                  name="last_name"
-                  required
-                  value={userUpdate.last_name}
-                />
-                <label htmlFor="first_name">Apellido</label>
-              </div>
+            <div className="form-floating mb-3">
+              <input
+                className="form-control form-control-sm"
+                onChange={handleChange}
+                type="text"
+                placeholder="Apellido"
+                name="last_name"
+                required
+                value={userUpdate.last_name}
+              />
+              <label htmlFor="last_name">Apellido</label>
+            </div>
 
-              <div className="form-floating mb-3">
-                <input
-                  className="form-control form-control-sm"
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Email"
-                  name="email"
-                  required
-                  value={userUpdate.email}
-                />
-                <label htmlFor="first_name">Email</label>
-              </div>
+            <div className="form-floating mb-3">
+              <input
+                className="form-control form-control-sm"
+                onChange={handleChange}
+                type="text"
+                placeholder="Email"
+                name="email"
+                required
+                value={userUpdate.email}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
 
-              <div className="form-floating mb-3">
-                <input
-                  className="form-control form-control-sm"
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Usuario Urbano"
-                  name="code_technical"
-                  required
-                  value={userUpdate.code_technical}
-                />
-                <label htmlFor="first_name">Usuario Urbano</label>
-              </div>
+            <div className="form-floating mb-3">
+              <input
+                className="form-control form-control-sm"
+                onChange={handleChange}
+                type="text"
+                placeholder="Usuario Urbano"
+                name="code_technical"
+                required
+                value={userUpdate.code_technical}
+              />
+              <label htmlFor="code_technical">Usuario Urbano</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                className="form-control form-control-sm"
+                onChange={handleChange}
+                type="text"
+                name="imageUrl"
+                required
+                value={userUpdate.imageUrl}
+              />
+              <label htmlFor="code_technical">URL imagen Perfil</label>
+            </div>
 
+            <div className="form-floating">
               <select
                 name="role"
                 className="form-select form-select-sm mb-3"
                 onChange={handleChange}
               >
-                <option value="">Actualizar Rol</option>
+                <option value={userUpdate.role}>{userUpdate.role}</option>
                 <option value="technical">Tecnico</option>
                 <option value="saler">Vendedor</option>
                 <option value="premium">Premium</option>
+                <option value="user">User</option>
               </select>
+              <label htmlFor="role">Role</label>
+            </div>
 
+            <div className="div d-flex justify-content-between">
               <button className="btn btn-primary" onClick={update}>
                 Update
               </button>
-            </form>
-          )}
-        </div>
+
+              <button
+                className="btn btn-danger ms-5"
+                onClick={() => remove(userUpdate)}
+              >
+                Remove
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -39,6 +39,7 @@ export default function Users() {
           <table className="table">
             <thead>
               <tr>
+                <th scope="col"></th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Email</th>
                 <th scope="col">Codigo Urbano</th>
@@ -51,6 +52,32 @@ export default function Users() {
                 users.map((user) => {
                   return (
                     <tr key={user._id}>
+                      <th>
+                        {" "}
+                        {user?.imageUrl ? (
+                          <img
+                            className="img-profile-thumb"
+                            src={user.imageUrl}
+                            alt="image profile"
+                          />
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="50"
+                            height="50"
+                            fill="currentColor"
+                            color="black"
+                            className="bi bi-person-circle"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                            <path
+                              fillRule="evenodd"
+                              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                            />
+                          </svg>
+                        )}
+                      </th>
                       <th>
                         {user.first_name} {user.last_name}
                       </th>
