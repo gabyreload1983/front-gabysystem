802e6b315384a457f29c2aebdca50688baca8741
Gabriel Godoy <gabyreload@gmail.com>
2024-01-24 09:07:56 -0300

    AccountDetail component

--- src/pages/Alexis/AccountDetail.jsx
+++ src/pages/Alexis/AccountDetail.jsx
@@ -1,13 +1,16 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "../../context/userContext";
-import { SwalError } from "../../utils";
+import { SwalError, SwalToast, SwalWaiting, formatPrice } from "../../utils";
+import moment from "moment";
+import Swal from "sweetalert2";
 
 export default function AccountDetail() {
   const { id } = useParams();
   const { logoutUserContext } = useContext(UserContext);
   const [item, setItem] = useState(null);
+  const navigate = useNavigate();
 
   const getItemDetail = async () => {
     try {
@@ -37,18 +40,58 @@ export default function AccountDetail() {
     getItemDetail();
   }, [id]);
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setSale((prevSale) => ({
-      ...prevSale,
-      [name]: value,
-    }));
+  const removeItem = async () => {
+    const answer = await Swal.fire({
+      text: `Borrar item???`,
+      showCancelButton: true,
+      confirmButtonText: "Aceptar",
+    });
+
+    if (!answer.isConfirmed) return;
+
+    SwalWaiting("Wait...");
+
+    try {
+      const response = await axios.delete(
+        `http://${import.meta.env.VITE_URL_HOST}/api/alexis/account/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          },
+        }
+      );
+
+      if (response?.data?.status === "success") {
+        SwalToast(response.data.message);
+        return navigate("/alexis/account");
+      }
+      SwalToast("ALgo salio mal. Contactar al administrador");
+    } catch (error) {
+      console.log(error);
+      SwalError(error);
+      if (error?.response?.status === 403) {
+        logoutUserContext();
+      }
+    }
   };
 
   return (
     <>
-      <h2>AccountDetail </h2>
-      {item && item.internalId}
+      <h2 className="text-center">DETALLE</h2>
+      {item && (
+        <div className="row gap-2 justify-content-center">
+          <div className="col-12 col-md-6 col-lg-3 p-3 border rounded-3 bg-light">
+            <p>FECHA: {moment(item.createdAt).format("DD-MM-YYYY")}</p>
+            <p>COMPROBANTE: {item.internalId}</p>
+            <p>IMPORTE: ${formatPrice(item.value)}</p>
+            <div className="row">
+              <button className="btn btn-outline-danger" onClick={removeItem}>
+                x
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
