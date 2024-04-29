abbc2fd838caec9aaec8396cc58bcb47c6f69dc0
Gabriel Godoy <gabyreload@gmail.com>
2024-04-29 11:09:05 -0300

    SummariesList

--- src/pages/Summaries/SummariesList.jsx
+++ src/pages/Summaries/SummariesList.jsx
@@ -8,35 +8,33 @@ export default function SummariesList({
   onHandleChangePhone,
 }) {
   return (
-    <>
-      <div className="col-12">
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">CODIGO</th>
-              <th scope="col">CLIENTE</th>
-              <th scope="col">SALDO</th>
-              <th scope="col">TIPO</th>
-              <th scope="col">EMAIL</th>
-              <th scope="col"></th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {customers.length > 0 &&
-              customers.map((customer) => (
-                <tr key={customer.codigo}>
-                  <SummaryItem
-                    customer={customer}
-                    onHandleChangeEmail={onHandleChangeEmail}
-                    onHandleEmailSummary={onHandleEmailSummary}
-                    onHandleChangePhone={onHandleChangePhone}
-                  />
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+    <div className="table-responsive">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">CODIGO</th>
+            <th scope="col">CLIENTE</th>
+            <th scope="col">SALDO</th>
+            <th scope="col">TIPO</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.length > 0 &&
+            customers.map((customer) => (
+              <tr key={customer.codigo}>
+                <SummaryItem
+                  customer={customer}
+                  onHandleChangeEmail={onHandleChangeEmail}
+                  onHandleEmailSummary={onHandleEmailSummary}
+                  onHandleChangePhone={onHandleChangePhone}
+                />
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
