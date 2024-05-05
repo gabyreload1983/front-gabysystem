95d2f32c824450961aaf374388c9472f5a667d44
Gabriel Godoy <gabyreload@gmail.com>
2024-05-05 00:02:20 -0300

    Loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,5 @@
+import { BarLoader } from "react-spinners";
+
+export default function Loading() {
+  return <BarLoader color="#36d7b7" cssOverride={{ width: "100%" }} />;
+}
---
+++ src/pages/ServiceWork/ServiceWorkLinks.jsx
@@ -0,0 +1,74 @@
+import { useContext, useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import { getInfoAllPendingServiceWorks } from "../../utils";
+import {
+  ClipboardDocumentListIcon,
+  ComputerDesktopIcon,
+  CubeIcon,
+  PrinterIcon,
+} from "@heroicons/react/16/solid";
+import Loading from "../../components/Loading";
+
+export default function ServiceWorkLinks() {
+  const { user } = useContext(UserContext);
+  const [serviceWorksInfo, setServiecWorkInfo] = useState(null);
+
+  const getData = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    const data = await getInfoAllPendingServiceWorks({ user });
+    setServiecWorkInfo(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const links = [
+    {
+      name: "Pc",
+      to: "pc",
+      icon: ComputerDesktopIcon,
+      quantity: serviceWorksInfo ? serviceWorksInfo["pc"] : 0,
+    },
+    {
+      name: "Impresoras",
+      to: "printers",
+      icon: PrinterIcon,
+      quantity: serviceWorksInfo ? serviceWorksInfo["printers"] : 0,
+    },
+    {
+      name: "Proceso",
+      to: "process",
+      icon: ClipboardDocumentListIcon,
+      quantity: serviceWorksInfo ? serviceWorksInfo["process"] : 0,
+    },
+    {
+      name: "Mis Ordenes",
+      to: "my-works",
+      icon: CubeIcon,
+      quantity: serviceWorksInfo ? serviceWorksInfo["myWorks"] : 0,
+    },
+  ];
+
+  if (!serviceWorksInfo) return <Loading />;
+
+  return (
+    <>
+      {links.map((link) => {
+        const Icon = link.icon;
+        return (
+          <NavLink
+            key={link.name}
+            className="navLink flex-fill p-2 d-flex align-items-center gap-2 flex-row justify-content-center justify-content-xl-start"
+            to={link.to}
+          >
+            <Icon className="icon" />
+            <p className="d-none d-xl-block m-0">{link.name}</p>
+            <span className="bg-info px-2 py-1 rounded">{link.quantity}</span>
+          </NavLink>
+        );
+      })}
+    </>
+  );
+}
