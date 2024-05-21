6d2690f9e4e3cdc77fb113845a52f91bf7f2e28c
Gabriel Godoy <gabyreload@gmail.com>
2024-05-21 10:36:47 -0300

    7.16.4 repair statistics (#56)
    * Start StatisticsRepairs
    
    * CalendarPicker and filter statistics by sector
    
    * StatisticsRepaired
    
    * install react-google-charts
    
    * CalendarPicker responsive
    
    * getSectorStatistics
    
    * getStatisticsRepairPending getStatisticsInOut
    
    * sync query params
    
    * delete unnecessary components

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,32 @@
-export const API_URL = "http://192.168.8.151:3400";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "http://192.168.8.151:3400"
+    : "http://localhost:3400";
+
+export const tiers = [
+  "0-NORMAL",
+  "1",
+  "2",
+  "3-ARMADOS",
+  "4-TURNOS/PRIORIDADES",
+  "5-GARANTIA REPARACION",
+  "6",
+  "7",
+  "8-BOXES",
+  "9-ABONADOS",
+  "10-GARANTIA COMPRA",
+];
+
+export const colorsTiers = [
+  "#c4c7b4",
+  "#727950",
+  "#e4f58d",
+  "#d3e910",
+  "#54c0db",
+  "#ffc107",
+  "#1fe66b",
+  "#a83e73",
+  "#f02727",
+  "#fd7e14",
+  "#ff0000",
+];
--- src/pages/Statistics/Repairs/StatisticsRepairs.jsx
+++ src/pages/Statistics/Repairs/StatisticsRepairs.jsx
@@ -1,3 +1,107 @@
+import { useState } from "react";
+import { getSectorStatistics, getServiceWorks } from "../../../utils";
+import Loading from "../../../components/Loading";
+import CalendarPicker from "../../../components/CalendarPicker";
+import moment from "moment";
+import PieGraph from "../../../components/PieGraph";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 export default function StatisticsRepairs() {
-  return <div>StatisticsRepairs</div>;
+  const navigate = useNavigate();
+  const now = moment().format("YYYY-MM-DD");
+  const [searchParams] = useSearchParams();
+  const from = searchParams.get("from") || now;
+  const to = searchParams.get("to") || now;
+
+  const [serviceWorksPc, setServiceWorksPc] = useState(false);
+  const [serviceWorksPrinter, setServiceWorksPrinter] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [calendar, setCalendar] = useState({
+    from: from,
+    to: to,
+  });
+
+  const getData = async () => {
+    setLoader(true);
+    const data = await getServiceWorks(calendar.from, calendar.to);
+
+    if (!data || data?.length === 0) return setLoader(false);
+
+    const { dataPie: dataPiePc, options: optionsPc } = getSectorStatistics({
+      data,
+      sector: ".PC",
+    });
+    const { dataPie: dataPiePrinter, options: optionsPrinter } =
+      getSectorStatistics({ data, sector: ".IMP" });
+
+    setServiceWorksPc({ dataPiePc, optionsPc });
+    setServiceWorksPrinter({ dataPiePrinter, optionsPrinter });
+    setLoader(false);
+  };
+
+  const handleCalendarChange = (e) => {
+    const { value, name } = e.target;
+    const copyCalendar = { ...calendar };
+    copyCalendar[name] = value;
+
+    setCalendar(copyCalendar);
+    navigate(
+      `/statistics/repairs?from=${copyCalendar.from}&to=${copyCalendar.to}`
+    );
+  };
+
+  useState(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="container">
+      {loader && <Loading />}
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-4 col-lg-3 p-2">
+          <CalendarPicker
+            label="from"
+            value={calendar.from}
+            handleOnChange={handleCalendarChange}
+          />
+        </div>
+        <div className="col-12 col-md-4 col-lg-3 p-2">
+          <CalendarPicker
+            label="to"
+            value={calendar.to}
+            handleOnChange={handleCalendarChange}
+          />
+        </div>
+      </div>
+      <div className="row justify-content-center mt-3">
+        <div className="col-12 col-md-8 col-lg-6">
+          <button className="btn btn-info w-100" onClick={getData}>
+            Aceptar
+          </button>
+        </div>
+      </div>
+      <div className="row mt-3">
+        {serviceWorksPc && (
+          <>
+            <div className="col-12 col-lg-6 p-2">
+              <PieGraph
+                data={serviceWorksPc.dataPiePc}
+                options={serviceWorksPc.optionsPc}
+              />
+            </div>
+          </>
+        )}
+        {serviceWorksPrinter && (
+          <>
+            <div className="col-12 col-lg-6 p-2">
+              <PieGraph
+                data={serviceWorksPrinter.dataPiePrinter}
+                options={serviceWorksPrinter.optionsPrinter}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
