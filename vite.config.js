625d31083024593a04e3c333736df360be9ac551
Gabriel Godoy <gabyreload@gmail.com>
2024-06-07 19:47:46 -0300

    7.23.5-bug-env-variables

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { readFileSync } from "fs";
 import { resolve } from "path";
@@ -7,11 +7,9 @@ const packageJson = JSON.parse(
   readFileSync(resolve(__dirname, "package.json"), "utf-8")
 );
 
-const env = loadEnv(process.env.NODE_ENV, process.cwd());
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env.PACKAGE_VERSION": JSON.stringify(packageJson.version),
-    "process.env.API_URL": JSON.stringify(env.VITE_API_URL),
   },
 });
