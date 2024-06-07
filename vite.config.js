1b03884e541602dd5adb42498172133b7858c9f2
Gabriel Godoy <gabyreload@gmail.com>
2024-06-07 12:17:01 -0300

    vite.config env variables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
+const packageJson = JSON.parse(
+  readFileSync(resolve(__dirname, "package.json"), "utf-8")
+);
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd());
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.PACKAGE_VERSION": JSON.stringify(packageJson.version),
+    "process.env.API_URL": JSON.stringify(env.VITE_API_URL),
+  },
+});
