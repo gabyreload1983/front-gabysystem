98c33c449cea08923b955dd2de612c10110742ec
Gabriel Godoy <gabyreload@gmail.com>
2024-07-18 15:26:34 -0300

    7.32.8-refactoring-unnecessary-files

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="d-flex flex-column jsutify-content-center align-items-center">
-      <h6>GSystem</h6>
-    </footer>
-  );
-}
