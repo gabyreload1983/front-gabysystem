bb7108098b815d0ad595a343c32728cfe130342f
Gabriel Godoy <gabyreload@gmail.com>
2024-08-02 12:23:32 -0300

    get views of alexisProfit

--- src/pages/Alexis/SalesList.jsx
+++ src/pages/Alexis/SalesList.jsx
@@ -9,9 +9,18 @@ import {
 } from "../../utils";
 
 export default function SalesList({ sales }) {
-  console.log(sales);
   return (
     <>
+      <div className="row bg-success">
+        <div className="col d-none d-lg-block">FECHA</div>
+        <div className="col col-lg">CLIENTE</div>
+        <div className="col col-lg">COMPROBANTE</div>
+        <div className="col d-none d-lg-block">IMPORTE</div>
+        <div className="col-2 col-lg">SALDO</div>
+        <div className="col d-none d-lg-block">FECHA PAGO</div>
+        <div className="col d-none d-lg-block">OC</div>
+        <div className="col-2 col-lg">ENTREGADO</div>
+      </div>
       {sales.length &&
         sales.map((sale) => (
           <Link
---
+++ src/utils/alexis.js
@@ -0,0 +1,43 @@
+import moment from "moment";
+import { getFromApi, postToApi } from "../utils";
+import { API_URL } from "../constants";
+const RENT_PORCENTAGE = 0.4;
+const CHECH_TAX_PORCENTAGE = 1.2 / 100;
+
+export const getAlexisSalesFrom = async (year) => {
+  const from = moment().format(`${year}-01-01`);
+  const to = moment().format(`${year}-12-31`);
+
+  const response = await getFromApi(
+    `${API_URL}/api/alexis/sales?from=${from}&to=${to}`
+  );
+
+  if (!response) return;
+  return response.payload
+    .filter((sale) => sale.type === "FV" && sale.isValid)
+    .toSorted((a, b) => new Date(b.date) - new Date(a.date));
+};
+
+export const updateAlexisSalesFrom = async (year) => {
+  const body = {
+    from: moment().format(`${year}-01-01 00:00:00`),
+    to: moment().format(`${year}-12-31 23:59:59`),
+  };
+
+  return await postToApi(`${API_URL}/api/alexis/sales/refresh`, body);
+};
+
+export const getSalesToFree = (sales) =>
+  sales.filter((sale) => sale.invoiceState === "toFree");
+
+export const getSalesToFreeTotal = (sales) =>
+  getSalesToFree(sales).reduce((acc, val) => acc + getAlexisProfit(val), 0);
+
+export const getAlexisProfit = (sale) => {
+  const TOTAL_INVOICE = sale.subTotal + sale.tax;
+  const CHECK_TAX = TOTAL_INVOICE * CHECH_TAX_PORCENTAGE;
+  const PROFIT = sale.profit - CHECK_TAX;
+  return sale.deliveryCost === 0
+    ? PROFIT * RENT_PORCENTAGE
+    : PROFIT * RENT_PORCENTAGE - sale.deliveryCost / 2;
+};
