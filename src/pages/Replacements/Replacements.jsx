b072f764687809cb7d0164b6e5616b74e03ba657
Gabriel Godoy <gabyreload@gmail.com>
2024-11-15 13:16:55 -0300

    Table Replacements

--- src/pages/Replacements/Replacements.jsx
+++ src/pages/Replacements/Replacements.jsx
@@ -3,13 +3,20 @@ import moment from "moment";
 import { UserContext } from "../../context/userContext";
 import { useContext, useEffect, useState } from "react";
 import { getReplacements } from "../../utils/data";
+import Table from "../../components/TableReplacements/Table";
 
 export default function Replacements() {
   const { user } = useContext(UserContext);
   const [replacements, setReplacements] = useState([]);
+  const [sortData, setSortData] = useState({
+    name: "fecha",
+    code: "createdAt",
+    sort: true,
+  });
 
   const getData = async () => {
     const data = await getReplacements();
+
     setReplacements(data);
   };
 
@@ -17,87 +24,57 @@ export default function Replacements() {
     getData();
   }, []);
 
+  const columns = [
+    { name: "#", code: "position", sort: false },
+    { name: "fecha", code: "createdAt", sort: true },
+    { name: "orden", code: "orderNumber", sort: false },
+    { name: "tecnico", code: "technical_code", sort: false },
+    { name: "descripcion", code: "description", sort: false },
+    { name: "prov.", code: "supplier", sort: false },
+    { name: "costo", code: "cost", sort: false },
+    { name: "precio", code: "finalPrice", sort: false },
+    { name: "demora", code: "delay", sort: false },
+    { name: "envio", code: "shipmment", sort: false },
+    { name: "confirmacion", code: "customerConfirmation", sort: false },
+    { name: "estado", code: "status", sort: false },
+    { name: "link", code: "linkSupplier", sort: false },
+  ];
+
+  const sortDataBy = (column) => {
+    function compareDates(a, b) {
+      if (a[column.code] < b[column.code]) {
+        return sortData.sort ? -1 : 1;
+      }
+      if (a[column.code] > b[column.code]) {
+        return sortData.sort ? 1 : -1;
+      }
+      return 0;
+    }
+    setReplacements((prev) => prev.sort(compareDates));
+
+    setSortData((prev) => ({ ...column, sort: !prev.sort }));
+  };
+
   return (
-    <div className="table-responsive">
-      <table className="table table-sm">
-        <thead>
-          <tr className="table-light">
-            <th>#</th>
-            <th className="d-none d-lg-table-cell">Nro ORDEN</th>
-            <th className="d-none d-md-table-cell">TECNICO</th>
-            <th className="d-none d-lg-table-cell">DESCRIPCION</th>
-            <th className="d-none d-lg-table-cell">IMAGENES</th>
-            <th className="d-none d-lg-table-cell">CODIGO PROV</th>
-            <th className="d-none d-lg-table-cell">PROVEEDOR</th>
-            <th className="d-none d-lg-table-cell">COSTO</th>
-            <th className="d-none d-lg-table-cell">PRECIO FINAL</th>
-            <th className="d-none d-lg-table-cell">DEMORA</th>
-            <th className="d-none d-lg-table-cell">ENVIO</th>
-            <th className="d-none d-lg-table-cell">CONFIRMACION PROV</th>
-            <th className="d-none d-lg-table-cell">ESTADO</th>
-            <th className="d-none d-lg-table-cell">LINK</th>
-            <th className="d-none d-lg-table-cell">NOTAS</th>
-          </tr>
-        </thead>
-        <tbody>
-          {replacements.length > 0 &&
-            replacements.map((replacement, index) => {
-              return (
-                <tr key={`${replacement._id}`}>
-                  <td>{index + 1}</td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.orderNumber}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.technical_code}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.description}
-                  </td>
-                  <td className="d-none d-lg-table-cell custom-td">
-                    {replacement.images}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.supplier_code}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.supplier}
-                  </td>
-                  <td className="d-none d-lg-table-cell">{replacement.cost}</td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.finalPrice}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.delay}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.shipmment}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.customerConfirmation
-                      ? "Confirmed"
-                      : "Not Confirmed"}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.status}
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    <a
-                      href={replacement.linkSupplier}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Supplier Link
-                    </a>
-                  </td>
-                  <td className="d-none d-lg-table-cell">
-                    {replacement.notes || "No notes available"}
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+    <div className="container-fluid">
+      <div className="row gap-3">
+        <div className="d-flex justify-content-end mt-3">
+          <button className="btn btn-success">Agregar</button>
+        </div>
+
+        <div className="col">
+          <div className="row">
+            {replacements && (
+              <Table
+                columns={columns}
+                data={replacements}
+                sortDataBy={sortDataBy}
+                sortData={sortData}
+              />
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
