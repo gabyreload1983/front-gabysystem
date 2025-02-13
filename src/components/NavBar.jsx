286ceb63287cc2a832f2fb300a34f7bceec8718c
Gabriel Godoy <gabyreload@gmail.com>
2025-02-13 15:30:10 -0300

    CreateOrder component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,17 @@
+import CreateOrder from "./CreateOrder";
 import SearchOrder from "./SearchOrder";
 
 function NavBar() {
   return (
     <nav className="navbar navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand m-auto" href="/">
+      <div className="container-fluid d-flex justify-content-around">
+        <a className="navbar-brand" href="/">
           <h1 className="m-0">GSystem</h1>
         </a>
-        <SearchOrder />
+        <div className="d-flex gap-2">
+          <CreateOrder />
+          <SearchOrder />
+        </div>
       </div>
     </nav>
   );
