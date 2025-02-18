20944993aed37e906190b9016124a2333e985c12
Gabriel Godoy <gabyreload@gmail.com>
2025-02-18 15:05:16 -0300

    adding totalServers property styling subscriber detail

--- src/pages/Subscribers/edit/UpdateSubscriber.jsx
+++ src/pages/Subscribers/edit/UpdateSubscriber.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getSubscriber, updateSubscriber } from "../../../utils/data";
 import { SwalToast } from "../../../utils/alerts";
 
 export default function UpdateSubscriber() {
   const { id } = useParams();
   const [subscriber, setSubscriber] = useState(null);
+  const navigate = useNavigate();
 
   const handleUpdateSubscriber = async () => {
     const totalEquipments = Number(
       document.getElementById("totalEquipments").value
     );
-    const subscriberUpdate = { ...subscriber, totalEquipments };
+    const totalServers = Number(document.getElementById("totalServers").value);
+    const subscriberUpdate = { ...subscriber, totalEquipments, totalServers };
 
     const response = await updateSubscriber(id, subscriberUpdate);
-    if (response) SwalToast(`Se actualizo cantidad de equipos!`, 1000);
+    if (response) {
+      await SwalToast(`Se actualizo cantidad de equipos!`, 500);
+      navigate(`/subscribers/detail/${id}`);
+    }
   };
   const getData = async () => {
     const data = await getSubscriber({ code: id });
@@ -27,34 +32,45 @@ export default function UpdateSubscriber() {
   }, []);
 
   return (
-    <div className="container">
-      <div className="row my-3 justify-content-center">
-        {subscriber && (
-          <div className="col-8">
-            <h2>{subscriber.name}</h2>
+    <>
+      {subscriber && (
+        <div className="col-12 col-md-6 bg-dark text-white p-3 rounded">
+          <h2>{subscriber.name}</h2>
 
-            <div className="form-floating mb-3">
-              <input
-                className="form-control form-control-sm"
-                id="totalEquipments"
-                defaultValue={subscriber.totalEquipments || 0}
-                type="text"
-                placeholder="Nombre"
-                name="totalEquipments"
-                required
-              />
-              <label htmlFor="totalEquipments">Total de Equipos</label>
-            </div>
-
-            <button
-              className="btn btn-primary"
-              onClick={handleUpdateSubscriber}
-            >
-              Actualizar
-            </button>
+          <div className="form-floating mb-3">
+            <input
+              className="form-control form-control-sm"
+              id="totalEquipments"
+              defaultValue={subscriber.totalEquipments || 0}
+              placeholder="Nombre"
+              name="totalEquipments"
+              required
+              type="number"
+              min={0}
+              max={100}
+            />
+            <label htmlFor="totalEquipments">Total de Equipos</label>
           </div>
-        )}
-      </div>
-    </div>
+          <div className="form-floating mb-3">
+            <input
+              className="form-control form-control-sm"
+              id="totalServers"
+              defaultValue={subscriber.totalServers || 0}
+              placeholder="Nombre"
+              name="totalServers"
+              required
+              type="number"
+              min={0}
+              max={100}
+            />
+            <label htmlFor="totalServers">Total de Servidores</label>
+          </div>
+
+          <button className="btn btn-primary" onClick={handleUpdateSubscriber}>
+            Actualizar
+          </button>
+        </div>
+      )}
+    </>
   );
 }
