d517f552d48f47a48276c7cfa5d7e178777b4416
Gabriel Godoy <gabyreload@gmail.com>
2025-02-25 16:15:29 -0300

    UploadImagesReplacement camera and gallery buttons

--- src/pages/ServiceWork/detail/UploadImagesReplacement.jsx
+++ src/pages/ServiceWork/detail/UploadImagesReplacement.jsx
@@ -1,39 +1,80 @@
 import { useState } from "react";
+import { SwalError } from "../../../utils/alerts";
 
 export default function UploadImagesReplacement({ onHandleUpload }) {
-  const [files, setFiles] = useState([]);
+  const [images, setImages] = useState([]);
 
-  const handleChange = (event) => {
-    const selectedFiles = Array.from(event.target.files);
-    if (selectedFiles.length > 0) {
-      setFiles(selectedFiles);
-    } else {
-      console.error("No files selected");
-    }
+  const handleImageChange = (event) => {
+    const files = Array.from(event.target.files);
+    setImages((prevImages) => [...prevImages, ...files]);
   };
+
   const handleUpload = async () => {
-    if (files.length === 0) {
-      console.error("No files selected");
-      return;
+    if (images.length === 0) {
+      return SwalError("Debe cargar al menos una foto.");
     }
 
     const formData = new FormData();
-    files.forEach((file) => formData.append("imagesReplacement", file));
+    images.forEach((image) => formData.append("imagesReplacement", image));
 
-    onHandleUpload(formData);
+    await onHandleUpload(formData);
+    setImages([]);
   };
+
   return (
-    <div className="border rounded mb-3 input-group">
+    <div className="border rounded p-3 d-flex justify-content-center flex-column gap-2 mb-3">
       <input
-        className="form-control"
+        id="galleryInput"
         type="file"
         accept="image/*"
         multiple
-        onChange={handleChange}
+        onChange={handleImageChange}
+        className="d-none"
       />
-      <button className="btn btn-success" onClick={handleUpload}>
-        Subir Imágenes
+      <input
+        id="cameraInput"
+        type="file"
+        accept="image/*"
+        capture="environment"
+        multiple
+        onChange={handleImageChange}
+        className="d-none"
+      />
+
+      <label
+        htmlFor="galleryInput"
+        className="custom-file-button btn btn-primary"
+      >
+        📁 Seleccionar de Galería
+      </label>
+
+      <label
+        htmlFor="cameraInput"
+        className="custom-file-button btn btn-success d-md-none"
+      >
+        📸 Tomar Foto
+      </label>
+
+      <button className="btn btn-warning" onClick={handleUpload}>
+        🚀 Subir Imágenes
       </button>
+
+      <div className="preview">
+        {images.map((image) => (
+          <img
+            key={image.name}
+            src={URL.createObjectURL(image)}
+            alt={`preview image`}
+            className="p-1"
+            style={{
+              width: 100,
+              height: 100,
+              objectFit: "cover",
+              borderRadius: 10,
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
