947063669d8fec78482f9c5a963269d2d3d93cfb
Gabriel Godoy <gabyreload@gmail.com>
2025-03-10 10:20:49 -0300

    addind search by phone and email styling component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,47 @@
 import React, { useState } from "react";
 
-export default function Search({
-  onSearch,
-  onClean,
-  searchDescription = "Buscar",
-}) {
+export default function Search({ onSearch, onClean }) {
+  const SEARCH_BY = {
+    DESCRIPTION: "description",
+    PHONE: "phone",
+    EMAIL: "email",
+  };
+  const PLACEHOLDER_SEARCH_BY = {
+    DESCRIPTION: "Descripcion",
+    PHONE: "Telefono",
+    EMAIL: "Email",
+  };
+
   const [input, setInput] = useState("");
+  const [serchBy, setSerchBy] = useState(SEARCH_BY.DESCRIPTION);
+  const [placeholder, setPlaceholder] = useState(
+    PLACEHOLDER_SEARCH_BY.DESCRIPTION
+  );
 
   const handleInputChange = (event) => {
     setInput(event.target.value);
   };
 
   const handleKeyDown = async (event) => {
-    const keyCode = event.keyCode;
-    if (keyCode === 13 && input.length >= 3) onSearch(input);
+    const { key } = event;
+
+    if (key === "Enter" && input.length >= 3) onSearch(serchBy, input);
+    if (key === "F4") {
+      setSerchBy((prev) => {
+        if (prev === SEARCH_BY.DESCRIPTION) return SEARCH_BY.PHONE;
+        if (prev === SEARCH_BY.PHONE) return SEARCH_BY.EMAIL;
+        if (prev === SEARCH_BY.EMAIL) return SEARCH_BY.DESCRIPTION;
+      });
+      setPlaceholder((prev) => {
+        if (prev === PLACEHOLDER_SEARCH_BY.DESCRIPTION)
+          return PLACEHOLDER_SEARCH_BY.PHONE;
+        if (prev === PLACEHOLDER_SEARCH_BY.PHONE)
+          return PLACEHOLDER_SEARCH_BY.EMAIL;
+        if (prev === PLACEHOLDER_SEARCH_BY.EMAIL)
+          return PLACEHOLDER_SEARCH_BY.DESCRIPTION;
+      });
+      setInput("");
+    }
   };
 
   const handleClick = async () => {
@@ -26,22 +54,28 @@ export default function Search({
   };
 
   return (
-    <div className="d-flex ms-auto mb-3 gap-2">
+    <div className="p-3 border rounded">
       <input
         className="form-control me-2"
         type="search"
-        placeholder={searchDescription}
+        placeholder={`${placeholder}`}
         onChange={handleInputChange}
         name="search"
         onKeyDown={handleKeyDown}
         value={input}
+        id="search"
       />
-      <button className="btn btn-success" onClick={handleClick}>
-        Buscar
-      </button>
-      <button className="btn btn-warning" onClick={handleClean}>
-        Limpiar
-      </button>
+      <label htmlFor="search" className="form-label mt-1">
+        F4 para cambiar busqueda
+      </label>
+      <div className="d-flex gap-2">
+        <button className="btn btn-success" onClick={handleClick}>
+          Buscar
+        </button>
+        <button className="btn btn-warning" onClick={handleClean}>
+          Limpiar
+        </button>
+      </div>
     </div>
   );
 }
